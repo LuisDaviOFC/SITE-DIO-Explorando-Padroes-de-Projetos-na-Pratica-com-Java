9f9050a0010aeb23dbf28d4bade98776e7dd545b
Luis Davi Vieira SIlva <85641308+LuisDaviOFC@users.noreply.github.com>
2023-03-30 18:09:21 -0300

    Projeto Finalizado

---
+++ spring/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.labpadroesprojetospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
---
+++ spring/lab-padroes-projeto-spring/src/main/java/one/digitalinnovation/labpadroesprojetospring/model/ClienteRepository.java
@@ -0,0 +1,6 @@
+package one.digitalinnovation.labpadroesprojetospring.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepository  extends CrudRepository<Cliente, Long> {
+}
